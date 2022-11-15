965fa5a5f256231e55a7c27b2f2aef757943db2b
E-Evgeniy <e.evgeniy.work@gmail.com>
2022-11-15 23:41:21 +0300

    begin

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_192944) do
+ActiveRecord::Schema.define(version: 2022_11_15_202652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
---
+++ spec/factories/products.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :product do
+    name { "MyString" }
+    comment { "MyString" }
+  end
+end
