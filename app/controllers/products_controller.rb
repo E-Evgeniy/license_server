cb5c583447038475663d88248c50fc1d184c5042
Evgeniy-ES <ees86@yandex.ru>
2022-11-16 16:01:05 +0300

    begin

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,42 @@
 class ProductsController < ApplicationController
+  before_action :load_product, only: %i[ show edit update]
+
   def index
     @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product=Product.new(product_params)
+    
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def load_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :comment)
   end
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :product do
     name { "MyString" }
     comment { "MyString" }
+
+    trait :invalid do
+      name { nil }
+    end
   end
 end
