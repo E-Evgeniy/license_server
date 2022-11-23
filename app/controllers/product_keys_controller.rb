dcbef05f84c027ace3581ebd8aada7db653672ae
Evgeniy-ES <ees86@yandex.ru>
2022-11-23 17:01:15 +0300

    product_key begin

--- app/controllers/product_keys_controller.rb
+++ app/controllers/product_keys_controller.rb
@@ -1,2 +1,10 @@
 class ProductKeysController < ApplicationController
+  def new(client_id)
+    @product_key = ProductKey.new
+    @product_key.client_id = client_id
+  end
+
+  def create
+    @product_key = ProductKey.new
+  end
 end
