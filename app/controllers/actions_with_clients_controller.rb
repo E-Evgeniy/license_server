b387e39b73290294fb1055a7e8c445731b88ccab
Evgeniy-ES <ees86@yandex.ru>
2022-11-28 16:52:37 +0300

    create key. Retunr work normal. Need change front

--- app/controllers/actions_with_clients_controller.rb
+++ app/controllers/actions_with_clients_controller.rb
@@ -1,17 +1,21 @@
 class ActionsWithClientsController < ApplicationController
 
-  def added_keys
-    @new_keys = []
+  def create_keys
+    new_keys = []
     number_keys = params[:number_keys].to_i.abs
     number_days = params[:number_days].to_i.abs
 
     return unless number_keys.positive? && (number_days.positive? || params[:infinite_key])
 
-    @new_keys << create_keys(params, @new_keys, number_keys, number_days)
+    new_keys << create_keys1(params, new_keys, number_keys, number_days)
+    new_keys.delete(new_keys.last)
+    
+    new_keys_json = new_keys.to_json
 
+    redirect_to controller: 'added_keys', action: 'added_keys', new_keys: new_keys_json
   end
 
-  def create_keys(params, new_keys, number_keys, number_days)
+  def create_keys1(params, new_keys, number_keys, number_days)
     if params[:key_type]['key_type_id'] == 'local'
       infinite_period = true
     else
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Rails.application.routes.draw do
   resources :clients
   resources :product_keys
 
+  resources :actions_with_clients
+
   root to: 'generals#index'
 
   get 'find', to: 'clients#find', as: :find
-  get 'added_keys', to: 'actions_with_clients#added_keys', as: :added_keys
+  get 'create_keys', to: 'actions_with_clients#create_keys', as: :create_keys
+  get 'added_keys', to: 'added_keys#added_keys', as: :added_keys
+  
   get 'detach_client', to: 'product_keys#detach_client', as: :detach_client
 end
