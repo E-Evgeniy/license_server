148951c9ed916b683b16a5fcd3143228242df10b
Evgeniy-ES <ees86@yandex.ru>
2022-11-29 16:57:36 +0300

    need button to main on page key_type index

--- app/controllers/key_types_controller.rb
+++ app/controllers/key_types_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Theis is controller for KeyTypes
 class KeyTypesController < ApplicationController
   before_action :load_key_type, only: %i[show edit update destroy]
 
@@ -5,37 +8,44 @@ class KeyTypesController < ApplicationController
     @key_types = KeyType.all
   end
 
+  def show; end
+
+  def edit; end
+
   def new
     @key_type = KeyType.new
   end
 
   def create
-    @key_type = KeyType.new
+    @key_type = KeyType.new(key_type_params)
 
     if @key_type.save
-      redirect_to @key_type
+      redirect_to key_types_path
     else
       render :new
     end
   end
 
-  def show
-    
+  def update
+    if @key_type.update(key_type_params)
+      redirect_to key_types_path
+    else
+      render :edit
+    end
   end
 
-  def find
-  end
-
-  def result
+  def destroy
+    @key_type.destroy
+    redirect_to key_types_path
   end
 
   private
 
-  def client_params
+  def key_type_params
     params.require(:key_type).permit(:name, :comment)
   end
 
-  def load_client
+  def load_key_type
     @key_type = KeyType.find(params[:id])
   end
 end
