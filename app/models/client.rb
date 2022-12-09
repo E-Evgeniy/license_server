34192608e590f93d88c8d799a786b273ab24bdcb
E-Evgeniy <e.evgeniy.work@gmail.com>
2022-12-09 07:33:15 +0300

    delete client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Client < ApplicationRecord
-    has_many :product_keys
+  has_many :product_keys, dependent: :destroy
 end
--- app/models/find_client.rb
+++ app/models/find_client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class FindClient < ApplicationRecord
   include StringRequest
 
